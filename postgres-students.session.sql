6781206ad2961a78ba4fdce49abcb66a9f381a7a
Polina <smivasilisa@gmail.com>
2024-04-22 18:08:34 +0200

    Primary key

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,1 +1,24 @@
-CREATE TABLE test();
+DROP TABLE messages;
+
+CREATE TABLE messages(
+  id serial PRIMARY KEY,
+  body text NOT NULL CHECK(body != ''),
+  author varchar(256) NOT NULL CHECK(author!=''),
+  created_at timestamp DEFAULT current_timestamp,
+  is_read boolean DEFAULT false
+);
+
+-- INSERT INTO messages VALUES('Hello John!', 'Me');
+
+INSERT INTO messages(author, body) VALUES
+('John', 'Hello.'),
+('Me', 'Go to cafe!'),
+('John', 'Go.');
+
+INSERT INTO messages(author, body) VALUES
+('Peter', 'Hello.'),
+('Peter', 'Hello.');
+
+INSERT INTO messages(id, body, author) VALUES (
+  NULL, 'Text 3', 'Message author 3'
+);
