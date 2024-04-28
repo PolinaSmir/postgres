048aae0ec9764c6d7843c443d4aafa414352c8be
Polina <smivasilisa@gmail.com>
2024-04-28 19:26:21 +0200

    Table books resolve

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,23 +1,27 @@
-CREATE TABLE coaches(
-  id serial PRIMARY KEY,
-  name varchar(256) NOT NULL CHECK(name != '')
-  -- team_id int REFERENCES teams(id)
-);
+CREATE TABLE books(
+  book_number serial PRIMARY KEY,
+  author varchar(64) NOT NULL CHECK(author != '') ,
+  name varchar(256) NOT NULL CHECK(name != ''),
+  year numeric NOT NULL,
+  видавництво text NOT NULL CHECK(видавництво != ''),
+  cathegory varchar(256),
+  synopsis text,
+  amount_of_primirniky int NOT NULL,
+  status boolean NOT NULL
+  );
 
-CREATE TABLE teams (
-  id serial PRIMARY KEY,
-  name varchar(256),
-  coach_id int REFERENCES coaches(id)
-);
+  --  CHECK(amount_of_primirniky > 0)
 
-ALTER TABLE coaches
-ADD COLUMN team_id int REFERENCES teams(id);
+  INSERT INTO books(author, name, year, видавництво, cathegory, synopsis, amount_of_primirniky, status) VALUES
+  ('Dostojevskij', 'Vojna i Mir', 1645, 'Literature Club', 'ds', 'sadddd', 1, true);
 
-ALTER
+  ALTER TABLE books
+  ADD CONSTRAINT "unique_name_or_author" UNIQUE(name, author);
 
-INSERT INTO coaches(name, team_id) VALUES
-('Ivanov', NULL);
+  ALTER TABLE books
+  ADD CONSTRAINT "books_amount_of_primirniky" CHECK(amount_of_primirniky > 0);
 
-INSERT INTO teams(name, coach_id) VALUES
-('Monreal', 1);
+  DROP TABLE books;
 
+  INSERT INTO books(author, name, year, видавництво, cathegory, synopsis, amount_of_primirniky, status) VALUES
+  ('ALEX', 'METRO', 2000, 'Literature Club', 'detective', 'some description', 0, false);
